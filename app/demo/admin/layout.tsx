66a1a7f8331a140a8a3cc830c71065c49866f914
Faizan021 <88651704+Faizan021@users.noreply.github.com>
2026-04-18 09:59:36 +0200

    Delete app/demo/admin/layout.tsx

--- app/demo/admin/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { DashboardShell } from "@/components/dashboard/shell";
-
-export default function DemoAdminLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <DashboardShell role="admin" basePath="/demo/admin" isDemo>
-      {children}
-    </DashboardShell>
-  );
-}
