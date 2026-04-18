740d4197038fe0c40380eb26aca45182a8b4e654
Faizan021 <88651704+Faizan021@users.noreply.github.com>
2026-04-18 18:25:44 +0200

    Update sparkles-icon.tsx

--- components/ui/sparkles-icon.tsx
+++ components/ui/sparkles-icon.tsx
@@ -1,1 +1,9 @@
-
+export function SparklesIcon() {
+  return (
+    <svg viewBox="0 0 24 24" fill="none" className="h-4 w-4" aria-hidden="true">
+      <path d="M12 3l1.2 3.3L16.5 7.5l-3.3 1.2L12 12l-1.2-3.3L7.5 7.5l3.3-1.2L12 3Z" fill="currentColor" />
+      <path d="M18.5 14l.7 1.8 1.8.7-1.8.7-.7 1.8-.7-1.8-1.8-.7 1.8-.7.7-1.8Z" fill="currentColor" />
+      <path d="M6 14.5l.9 2.2 2.2.9-2.2.9-.9 2.2-2.2-.9.9-2.2Z" fill="currentColor" />
+    </svg>
+  );
+}
